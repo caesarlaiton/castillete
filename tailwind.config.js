2786b66ae0c4fb34cf1436524e903f6db1af61ad
César Laiton <caesarlaiton@gmail.com>
2020-05-04 16:45:17 -0400

    Adding Entypo's icons.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  theme: {
-    screens: {
-      '2xs': '320px',
-      'xs': '440px',
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px'
-    }
-  }
+	theme: {
+		screens: {
+			'2xs': '320px',
+			'xs': '440px',
+			'sm': '640px',
+			'md': '768px',
+			'lg': '1024px',
+			'xl': '1280px'
+		}
+	}
 }
