5c6710c3ba064634bffbbee4de1d671c37208e53
César Laiton <caesarlaiton@gmail.com>
2020-05-31 10:15:36 -0400

    Installing NPM gulp-htmlmin.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require("gulp"),
 	log = require("fancy-log"),
-	critical = require("critical").stream;
+	critical = require("critical").stream,
+	htmlmin = require("gulp-htmlmin");
 
 gulp.task("critical", () => {
   return gulp
@@ -19,5 +20,11 @@ gulp.task("critical", () => {
     .on("error", err => {
       log.error(err.message);
     })
+    .pipe(gulp.dest("public"));
+});
+
+gulp.task("minify", () => {
+  return gulp.src("public/**/*.html")
+    .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(gulp.dest("public"));
 });
